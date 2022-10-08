2b9b3363764778c896e97a1a74fb2acb06045906
wanglongcai <wanglongcai@zrwlkj.com.cn>
2022-10-08 11:02:05 +0800

    地图

--- pages/map/index.vue
+++ pages/map/index.vue
@@ -1,19 +1,21 @@
 <template>
 	<view class="pageMain">
+		<view class="fixed">
 		<cu-custom bgColor="bgcolor" :isBack="true">
 			<block slot="backText">返回</block>
 			<block slot="content">交通信息</block>
 		</cu-custom>
+		</view>
 		<view class="info" style="">
-			<view class="" v-cloak>
-				<text class="title">地址:</text>{{details.address}}
+			<view class="" style="border-bottom: 1px solid #e3e3de;" v-cloak>
+				<text class="title">地址</text>:{{details.address}}
 			</view>
 			<view class="" v-cloak>
-				<text class="title">交通方式:</text>{{details.buses}}
+				<text class="title">交通方式</text>:{{details.buses}}
 			</view>
 			
 		</view>
-		<map id="myMap" style="width: 100vw; height: calc(100vh - 44px);" :latitude="latitude" :longitude="longitude" :markers="markers" show-location></map>
+		<map id="myMap" style="width: 100vw; height: 100%;" :latitude="latitude" :longitude="longitude" :markers="markers" show-location></map>
 	</view>
 </template>
 
@@ -61,23 +63,33 @@ export default {
 <style lang="less" scoped>
 .pageMain {
 	position: relative;
+	height: 100vh;
 }
 .info {
 	font-weight: 600;
+	height: 80px;
 	background-color: white;
 	position: absolute;
+		left: 2%;
 	bottom: 20px;
 	width: 96%;
 	padding: 10px 30px;
-	border-radius: 20px;
-	left: 2%;
+	border-radius: 10px;
+
 	z-index: 2000;
 	box-shadow: 4px 4px 7px #c3c3c3;
 }
+.fixed {
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: 200;
+}
 .title {
-	width: calc(4em + 3px);
-	text-align: right;
-	line-height: 20px;
+	width: calc(4rem + 6px);
+	text-align: justify;
+	line-height: 30px;
 	font-weight: 500;
+	display: inline-block;
 }
 </style>
